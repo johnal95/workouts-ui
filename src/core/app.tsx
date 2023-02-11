55b4c0640fa20c4fe6538f347f0bb726d15ef735
Joao Afonso Gaspar Lopes <joao.afonso.1995@gmail.com>
2023-02-12 00:59:29 +0100

    Add react helmet

--- src/core/app.tsx
+++ src/core/app.tsx
@@ -1,6 +1,14 @@
 import * as React from "react";
-import { ReactElement } from "react";
+import { Fragment, ReactElement } from "react";
+import { Helmet } from "react-helmet";
 
-const App = (): ReactElement => <h1>Hello World!</h1>;
+const App = (): ReactElement => (
+    <Fragment>
+        <Helmet>
+            <title>Workouts</title>
+        </Helmet>
+        <h1>Hello World!</h1>
+    </Fragment>
+);
 
 export { App };
